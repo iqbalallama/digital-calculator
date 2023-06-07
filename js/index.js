ca0427ad0d7b28a6c4a25a8c0444647bdd0380cf
iqbalallama <iqbalallama201@gmail.com>
2023-06-07 16:33:46 +0600

    third

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 document.getElementById('calculator-body').addEventListener('click',function(e){
-    console.log(e.target.innerText)
+    const display = document.getElementById('display');
+    const displayValue = display.value ;
+    const currentNumber = e.target.innerText;
+    const newNumber = displayValue + currentNumber;
+    if(isNaN(currentNumber)){
+        if(currentNumber == 'AC'){
+            display.value = ''
+        }
+        else if(currentNumber == 'DC'){
+            display.value = display.value.toString().slice(0,-1);
+        }
+        else if(currentNumber == '='){
+            display.value = eval(display.value);
+        }
+        else{
+            display.value = newNumber;
+        }
+    }
+    else{
+        display.value = newNumber;
+    }
+    
 })
