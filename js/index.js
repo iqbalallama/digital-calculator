b435ff040b8e296c411fe2bd181d298dfd73221a
iqbalallama <iqbalallama201@gmail.com>
2023-06-07 21:18:55 +0600

    last

--- js/index.js
+++ js/index.js
@@ -1,25 +1,35 @@
+// calculator body
 document.getElementById('calculator-body').addEventListener('click',function(e){
+    // display part
     const display = document.getElementById('display');
     const displayValue = display.value ;
     const currentNumber = e.target.innerText;
+    // display new number
     const newNumber = displayValue + currentNumber;
+    // condition check
     if(isNaN(currentNumber)){
+        // all clear
         if(currentNumber == 'AC'){
             display.value = ''
         }
+        // clear 1 by 1
         else if(currentNumber == 'DC'){
             display.value = display.value.toString().slice(0,-1);
         }
+        // calculation part
         else if(currentNumber == '='){
             display.value = eval(display.value);
         }
+        // calculation part
         else if(currentNumber == '%'){
             display.value = eval(display.value / 100);
         }
+        // else part
         else{
             display.value = newNumber;
         }
     }
+    // display else part
     else{
             display.value = newNumber;
     }
